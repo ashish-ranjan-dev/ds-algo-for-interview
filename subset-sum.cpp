cc848c7e27f937439b6a2604bc6777a194cae8ce
afeefaa333 <afeefaa333@gmail.com>
2020-10-04 21:37:25 +0530

    Added C++ program for subset sum problem

---
+++ subset-sum.cpp
@@ -0,0 +1,30 @@
+//subset sum problem 
+#include <stdio.h> 
+bool isSubsetSum(int set[], int n, int sum) 
+{ 
+    if (sum == 0) 
+        return true; 
+    if (n == 0) 
+        return false; 
+  
+    // If last element is greater than sum
+    if (set[n - 1] > sum) 
+        return isSubsetSum(set, n - 1, sum); 
+  
+    // else, check whether the last element can be included
+    return isSubsetSum(set, n - 1, sum) 
+           || isSubsetSum(set, n - 1, sum - set[n - 1]); 
+} 
+  
+// Driver program
+int main() 
+{ 
+    int set[] = { 13,5,6,8,9,5 }; 
+    int sum = 26; 
+    int n = sizeof(set) / sizeof(set[0]); 
+    if (isSubsetSum(set, n, sum) == true) 
+        printf("Yes, there is a subset"); 
+    else
+        printf("No subset "); 
+    return 0;
+} 
